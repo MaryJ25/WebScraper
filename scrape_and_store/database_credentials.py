72898ea8d1b9ac4175b2ddf02609328ce76f1cbb
Mary Czapkowska <43907321+MaryJ25@users.noreply.github.com>
2021-05-18 22:19:20 +0300

    Delete database_credentials.py

--- scrape_and_store/database_credentials.py
+++
@@ -1,5 +0,0 @@
-DATABASE = "d8i0j90u4q6n4k",
-USER = "vqnngkqzkiedsf",
-PASSWORD = "efcc41ed610b8b8d323a1200d2ab89a22514cbd9f11670a02c34d26eb398df86",
-HOST = "ec2-54-155-87-214.eu-west-1.compute.amazonaws.com",
-PORT = "5432"
